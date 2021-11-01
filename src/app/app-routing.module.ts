178224de4f7aa21833c11c057400fef6d40e3b3a
Carlos Revilla <clro26.k@gmail.com>
2021-11-01 22:01:40 +0100

    routes path added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateTasksComponent } from './create-tasks/create-tasks.component';
+import { TaskComponent } from './task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskComponent },
+  { path: 'create-task', component: CreateTasksComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
+import { CreateTasksComponent } from './create-tasks/create-tasks.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskComponent],
+  declarations: [AppComponent, TaskComponent, CreateTasksComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
